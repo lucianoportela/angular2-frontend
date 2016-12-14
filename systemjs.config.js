39ffb8560d682318f92352552800b908f8e26ddb
Luciano Portela <portela.inf@gmail.com>
2016-12-14 09:21:15 -0200

    atualizando
    .js

--- systemjs.config.js
+++ systemjs.config.js
@@ -26,7 +26,7 @@
     // pacotes necessários
     packages: {
       app: {
-        main: './config/main.js',
+        main: './target/config/main.js',
         defaultExtension: 'js'
       },
       rxjs: {
